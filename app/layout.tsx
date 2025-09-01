3e1697d95851a87a0365760bc1b52037e8ba8efe
Jobair Al Sarkar <jobairalsarkar1@gmail.com>
2025-09-01 20:37:32 +0600

    Google Analytics - 01

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,7 +69,7 @@ export default function RootLayout({
       <head>
         {/* Google Analytics Script */}
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-SN9KDQZJD3"
+          src="https://www.googletagmanager.com/gtag/js?id=G-V0BXCV3KR2"
           strategy="afterInteractive"
         />
         <Script id="ga-init" strategy="afterInteractive">
@@ -77,7 +77,7 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-SN9KDQZJD3', {
+            gtag('config', 'G-V0BXCV3KR2', {
               page_path: window.location.pathname,
             });
           `}
